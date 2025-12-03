f326119382477a483807bf30c300da93548cd839
김용준 <netoda070@gmail.com>
2025-12-03 16:58:48 +0900

    주석처리
    주석처리

--- arduino/sensor_slave/cup_detect.ino
+++ arduino/sensor_slave/cup_detect.ino
@@ -1,24 +1,24 @@
 const int sensorPin = A0;  
-#const int ledPin = 13;     
+//const int ledPin = 13;     
 
 
 int threshold = 50; 
 
 void setup() {
-  #pinMode(ledPin, OUTPUT);
+  //pinMode(ledPin, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  #int rawValue = analogRead(sensorPin); // 0 ~ 1023 사이의 값 읽기
+  //int rawValue = analogRead(sensorPin); // 0 ~ 1023 사이의 값 읽기
 
 
   if (rawValue > threshold) {
-    #digitalWrite(ledPin, HIGH); // 아두이노 LED 켜기 (확인용)
+    //digitalWrite(ledPin, HIGH); // 아두이노 LED 켜기 (확인용)
     Serial.println("1");        // 파이썬으로 "1" (True) 전송
   } 
   else {
-    #digitalWrite(ledPin, LOW);  // 아두이노 LED 끄기
+    //digitalWrite(ledPin, LOW);  // 아두이노 LED 끄기
     Serial.println("0");        // 파이썬으로 "0" (False) 전송
   }
   
